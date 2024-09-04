1c282f7a8ef908c2cb473194a387d6d4bab0cb6a
Amunem0 <aryankashikar7@gmail.com>
2024-09-04 18:22:41 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1443
+let accountEmail = "aryankashikar7@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+// accountId=2 //not Allowed
+
+accountEmail = "kashikaraa@rknec.edu"
+accountPassword="212121"
+accountCity="Nagpur"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer version
+
+
+// alert("hello"); //we are using nodejs, not browser
+
+let name="Hitesh"
+let age=8
+let isLoggedIn =false;
+
+//number =>2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone
+// undefined => value is not assignedṇ
+// symbol => unique
+
+//object
+console.log(typeof(undefined)); //OUTPUT : undefined
+console.log(typeof(null)); //OUTPUT : Object

e4adf4052adfda849b15b3243dddf8e08daa57ab
Amunem0 <aryankashikar7@gmail.com>
2024-09-04 18:36:34 +0530

    first commit

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,19 @@
+let score= "33abd"
+
+console.log(typeof(score));
+
+let valueInNumber= Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+
+//"33" => 33
+//"33abc" => NaN
+//"true" => 1
+
+let isLoggedIn=1;
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+//1=>true ; 0=> false
+//"aryan"=>true ; ""=>false

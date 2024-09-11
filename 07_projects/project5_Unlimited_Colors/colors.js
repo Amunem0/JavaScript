ba9d359355705e78206f30a0796f99b8f59b4046
Amunem0 <aryankashikar7@gmail.com>
2024-09-11 14:44:46 +0530

    events and project commit

---
+++ 07_projects/project5_Unlimited_Colors/colors.js
@@ -0,0 +1,35 @@
+//generate a random color
+
+const randomColor=function(){
+    const hex="0123456789ABCDE";
+    let color="#";
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+console.log(randomColor());
+
+let intervalId;
+
+const startChangingColor=function(){
+    if(intervalId==null){
+        intervalId=setInterval(changeColor,1000);
+    }
+
+function changeColor(){
+        document.body.style.backgroundColor=randomColor();
+    }
+}
+    
+
+const stopChangingColor=function(){
+    clearInterval(intervalId);
+    intervalId=null;
+}
+
+document.querySelector("#start").addEventListener('click',startChangingColor)
+
+
+document.querySelector("#stop").addEventListener('click',stopChangingColor)

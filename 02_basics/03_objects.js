9f0f32b856e47edf35e1b4a9a43404ddce7c62a0
Aryan Kashikar <115734963+Amunem0@users.noreply.github.com>
2024-11-02 19:10:34 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,11 +1,18 @@
-const User={
-    name:"Aryan",
-    age:21,
-    email:"aryan&@gmail.com",
-    location:"Jaipur",
-    isLoggedIn:false,
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(User.email);
-console.log(User["email"]);
-
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
